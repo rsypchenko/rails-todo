e46591918b3b87b264432b038d433352d2e1ccb7
Roman Sypchenko <r.sipchenko@gmail.com>
2025-06-30 14:09:12 -0600

    Added reset form controller, small UI improvements

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,8 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import InlineEditController from "controllers/inline_edit_controller"
+import "controllers/reset_form_controller"
 
 eagerLoadControllersFrom("controllers", application)
 application.register("inline-edit", InlineEditController)
+application.register("reset-form", ResetFormController)
